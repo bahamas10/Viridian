48bf8b1afdd9d03378dfa9907fc07f4ccc6510fb
Dave Eddy <dave@daveeddy.com>
2012-06-01 09:32:16 -0700

    Moving locales into json strings files
    * Created it.json
    * Translations are a breeze now

--- locales/msgfmt.sh
+++
@@ -1,3 +0,0 @@
-rm -f locales/it/LC_MESSAGES/viridian.mo
-mkdir -p locales/it/LC_MESSAGES/
-msgfmt -o locales/it/LC_MESSAGES/viridian.mo locales/it.po
--- locales/xgettext.sh
+++
@@ -1,2 +0,0 @@
-xgettext -L Python -f locales/files.txt -o locales/messages.pot
-msgmerge -N locales/it.po locales/messages.pot -o locales/it.po
